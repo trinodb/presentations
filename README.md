3f4f219d523a49bda3922df3dea426cc5421390e
Manfred Moser <manfred@simpligility.ca>
2023-11-14 09:40:19 -0800

    Tweak SQL training material
    - Clean up some issues on data mgt
    - Clean up readme for repo
    - Add data mgt sql examples

--- README.md
+++ README.md
@@ -1,19 +1,26 @@
 # Trino presentations
 
-> **Note**
-> These presentations are in draft mode. Please check back when they are ready for full use!
+A collection of learning resources about Trino. The presentations can be used
+for any event and demo.
 
-A collection of learning resources about Trino.
+## Existing uses and further material
+
+* [What is Trino? at Trino Community Broadcast 48](https://trino.io/episodes/48)
+* [SQL training 1: Getting started with Trino and SQL](https://trino.io/blog/2023/10/18/sql-training-1)
+* [SQL training 2: Advanced analytics with SQL and Trino](https://trino.io/blog/2023/11/01/sql-training-2)
+* [SQL training 3: Data management with SQL and Trino](https://trino.io/blog/2023/11/15/sql-training-3)
 
 ## Presenting
 
-Use the URL for the repository to display the overview presentation, and
-navigate to the individual presentations.
+Use the [URL for the repository to display the overview
+presentation](https://trinodb.github.io/presentations/), and navigate to the
+individual presentation.
 
-Alternatively run a local webserver with the runLocalServer.sh or
-runLocalServer.cmd scripts, and navigate to http://localhost:9000.
+Alternatively, run a local webserver with the `runLocalServer.sh` or
+`runLocalServer.cmd` scripts, and navigate to
+[http://localhost:9000](http://localhost:9000).
 
-Press ? for keyboard shortcuts, and find more information in the [reveal.js
+Press `?` for keyboard shortcuts, and find more information in the [reveal.js
 documentation](https://revealjs.com/).
 
 ## More information
@@ -27,12 +34,23 @@ documentation](https://revealjs.com/).
 
 ## Building and updating the custom theme
 
-Reveal.js has [multiple built-in themes](https://revealjs.com/themes/) but what's better is you can customize your own theme. Trino presentations use our own branding and coloring already, but they can always be improved. Here are the steps to update them.
+Reveal.js has [multiple built-in themes](https://revealjs.com/themes/) but
+what's better is you can customize your own theme. Trino presentations use our
+own branding and coloring already, but they can always be improved. Here are the
+steps to update them.
 
-We've created [trino-theme.scss](/css/trino-theme.scss) from the ```.scss``` files in [/reveal/css/theme/source](/reveal/css/theme/source/). It will be automatically compiled from Sass to CSS (see the [gulpfile](https://github.com/hakimel/reveal.js/blob/master/gulpfile.js)) when you run `npm run build -- css-themes`. To see all exposed variables to use in the scss files, reference the [/reveal/css/theme/template/exposer.scss](/reveal/css/theme/template/exposer.scss) file.
+We've created [trino-theme.scss](/css/trino-theme.scss) from the ```.scss```
+files in [/reveal/css/theme/source](/reveal/css/theme/source/). It will be
+automatically compiled from Sass to CSS (see the
+[gulpfile](https://github.com/hakimel/reveal.js/blob/master/gulpfile.js)) when
+you run `npm run build -- css-themes`. To see all exposed variables to use in
+the scss files, reference the
+[/reveal/css/theme/template/exposer.scss](/reveal/css/theme/template/exposer.scss)
+file.
 
-> **Note**
-> For more general css changes to the presentation that don't directly apply to the Trino Reveal theme, put that CSS in [/css/trino.css](/css/trino.css)
+> **Note** For more general css changes to the presentation that don't directly
+> apply to the Trino Reveal theme, put that CSS in
+> [/css/trino.css](/css/trino.css)
 
 Each theme file does four things in the following order:
 
@@ -40,10 +58,14 @@ Each theme file does four things in the following order:
 Shared utility functions.
 
 2. **Include [/reveal/css/theme/template/settings.scss](/reveal/css/theme/template/settings.scss)**
-Declares a set of custom variables that the template file (step 4) expects. Can be overridden in step 3.
+Declares a set of custom variables that the template file (step 4) expects. Can
+be overridden in step 3.
 
 3. **Override**
-This is where you override the default theme. Either by specifying variables (see [settings.scss](/reveal/css/theme/template/settings.scss) for reference) or by adding any selectors and styles you please.
+This is where you override the default theme. Either by specifying variables
+(see [settings.scss](/reveal/css/theme/template/settings.scss) for reference) or
+by adding any selectors and styles you please.
 
 4. **Include [/reveal/css/theme/template/theme.scss](/reveal/css/theme/template/theme.scss)**
-The template theme file which will generate final CSS output based on the currently defined variables.
+The template theme file which will generate final CSS output based on the
+currently defined variables.
--- presentations/sql-data-mgt/example-queries.sql
+++ presentations/sql-data-mgt/example-queries.sql
@@ -1,1 +1,250 @@
 SHOW CATALOGS;
+
+CREATE CATALOG brain USING memory;
+
+CREATE CATALOG tpch USING tpch;
+
+SHOW CATALOGS;
+
+DROP CATALOG brain;
+
+DROP CATALOG tpch;
+
+SHOW CATALOGS;
+
+CREATE SCHEMA brain.demo;
+
+SHOW SCHEMAS FROM brain;
+
+-- Important for the following statements
+-- Change to whatever catalog and schema you use for learning
+USE brain.demo;
+
+CREATE TABLE person (
+  firstname varchar(50),
+  lastname varchar(50),
+  dob date
+);
+
+INSERT INTO person (firstname, lastname, dob)
+VALUES
+('Manfred', 'Moser', DATE '1999-01-01'),
+('David', 'Phillips', DATE '2000-02-02');
+
+SELECT * FROM  person;
+
+SHOW CATALOGS;
+
+SHOW SCHEMAS FROM brain;
+
+SHOW TABLES FROM brain.demo;
+
+SHOW CREATE TABLE brain.demo.person;
+
+DESCRIBE brain.demo.person;
+
+SELECT * FROM system.metadata.catalogs;
+
+SELECT * FROM brain.information_schema.columns
+  WHERE table_schema = 'demo'
+  AND table_name = 'person';
+
+
+-- Module 2
+
+DROP TABLE person;
+
+CREATE TABLE person (
+  uuid uuid,
+  firstname varchar(50),
+  lastname varchar(50),
+  dob date,
+  create_ts timestamp
+);
+
+DESCRIBE person;
+
+INSERT INTO person (uuid, firstname, lastname, dob, create_ts)
+VALUES
+(uuid(), 'Manfred', 'Moser', DATE '1999-01-01', now()),
+(uuid(), 'David', 'Phillips', DATE '2000-02-02', now());
+
+SELECT * FROM person;
+
+SELECT
+  firstname || ' ' || lastname AS Name,
+  date_diff('year', dob, current_date) AS Age
+FROM person;
+
+CREATE TABLE region AS
+  SELECT * FROM tpch.tiny.region;
+
+SELECT * FROM region;
+
+CREATE TABLE nation AS
+  SELECT * FROM tpch.tiny.nation;
+
+SELECT * FROM nation;
+
+CREATE TABLE supplier_us
+AS
+  SELECT * FROM tpch.tiny.supplier
+  WHERE nationkey = 24;
+
+SELECT * FROM supplier_us;
+
+SELECT
+  nationkey,
+  count(*) AS supplier_count
+ FROM tpch.sf100.supplier
+ GROUP BY nationkey;
+
+CREATE TABLE supplier_per_nation AS
+  SELECT
+    nationkey,
+    count(*) AS supplier_count
+  FROM tpch.sf100.supplier
+  GROUP BY nationkey;
+
+SELECT * FROM supplier_per_nation;
+
+SELECT nation.name, supplier_per_nation.supplier_count
+FROM supplier_per_nation
+JOIN nation
+ON supplier_per_nation.nationkey = nation.nationkey;
+
+DROP TABLE supplier_per_nation;
+
+-- Run again
+INSERT INTO person (uuid, firstname, lastname, dob, create_ts)
+VALUES
+(uuid(), 'Manfred', 'Moser', DATE '1999-01-01', now()),
+(uuid(), 'David', 'Phillips', DATE '2000-02-02', now());
+
+SELECT * FROM person;
+
+--oops .. need to update
+-- might not work depending on connector support for UPDATE
+UPDATE person
+SET
+  firstname='Martin',
+  lastname='Traverso',
+  dob = DATE '2000-03-03'
+WHERE uuid = UUID '042cbe59-4b63-48d0-b6a7-f8a535e16784'; -- update to UUID value
+
+UPDATE person
+SET
+  firstname='Dain',
+  lastname='Sundstrom',
+  dob = DATE '2000-04-04'
+WHERE uuid = UUID 'ec625e61-b4a2-4ce3-a597-a1897ef8e525'; -- update to UUID value
+
+UPDATE person
+SET create_ts = now();
+
+SELECT * FROM person;
+
+DELETE FROM person
+WHERE uuid = UUID '8e11175e-fe32-41d9-8de3-57d903b39de5' OR
+ uuid = UUID '648c440f-4c3b-48a2-8a0b-f3f55d7c425d'; -- update to UUID values
+
+SELECT * FROM person;
+
+DELETE FROM person
+WHERE firstname='Manfred';
+
+ALTER TABLE person
+ADD COLUMN update_ts timestamp; -- fails!
+
+ALTER TABLE person
+ADD COLUMN update_ts timestamp(6);
+
+ALTER TABLE person
+ADD COLUMN role varchar(50);
+
+ALTER TABLE person
+ALTER COLUMN lastname SET DATA TYPE varchar(100);
+
+DESCRIBE person;
+
+CREATE TABLE person2 AS SELECT * FROM person;
+
+-- Run again
+INSERT INTO person2 (uuid, firstname, lastname, dob, create_ts, update_ts, role)
+VALUES
+(uuid(), 'Manfred', 'Moser', DATE '1999-01-01', now(), now(), 'Maintainer');
+
+SELECT * FROM person;
+
+SELECT * FROM person2;
+
+MERGE INTO person USING person2 AS p2
+ON (person.uuid = p2.uuid)
+  WHEN MATCHED
+    THEN UPDATE SET role = 'Co-creator', update_ts = now()
+   WHEN NOT MATCHED
+        THEN INSERT (uuid, firstname, lastname, dob, create_ts, update_ts, role)
+              VALUES(p2.uuid, p2.firstname, p2.lastname, p2.dob, p2.create_ts, p2.update_ts, p2.role);
+
+DELETE FROM person WHERE firstname = 'Manfred';
+
+UPDATE person SET role = null;
+
+UPDATE person SET role = 'Co-creator';
+
+SELECT * FROM person;
+
+
+-- Module 3 - Views and materialized views
+
+CREATE VIEW vw_supplier_us
+AS
+  SELECT * FROM tpch.tiny.supplier
+  WHERE nationkey = 24;
+
+SELECT * FROM vw_supplier_us;
+
+DROP VIEW vw_supplier_us;
+
+CREATE VIEW vw_supplier_per_nation AS
+  SELECT
+    nationkey,
+    count(*) AS supplier_count
+  FROM tpch.sf100.supplier
+  GROUP BY nationkey;
+
+SELECT * FROM vw_supplier_per_nation;
+
+DROP VIEW vw_supplier_per_nation;
+
+SELECT nation.name, vw_supplier_per_nation.supplier_count
+FROM vw_supplier_per_nation
+JOIN nation
+ON vw_supplier_per_nation.nationkey = nation.nationkey;
+
+CREATE MATERIALIZED VIEW mv_supplier_us
+AS
+  SELECT * FROM tpch.tiny.supplier
+  WHERE nationkey = 24;
+
+SELECT * FROM mv_supplier_us;
+
+DROP MATERIALIZED VIEW mv_supplier_us;
+
+CREATE MATERIALIZED VIEW mv_supplier_per_nation AS
+  SELECT
+    nationkey,
+    count(*) AS supplier_count
+  FROM tpch.sf100.supplier
+  GROUP BY nationkey;
+
+SELECT * FROM mv_supplier_per_nation;
+
+SELECT
+    nation.name,
+    mv_supplier_per_nation.supplier_count
+FROM mv_supplier_per_nation
+JOIN nation
+ON mv_supplier_per_nation.nationkey = nation.nationkey;
+
+DROP MATERIALIZED VIEW mv_supplier_per_nation;
